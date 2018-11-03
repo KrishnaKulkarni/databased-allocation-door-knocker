f70bbcc29ec4776043519c0476bcf75eb11b1ffa
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2018-11-03 14:07:08 -0400

    Fix indentation

--- script.py
+++ script.py
@@ -16,24 +16,24 @@ def run():
   return voter_df
 
 def voter_list(walk_universe):
-    sanitized_universe = __sanitize_walk_universe(walk_universe)
+  sanitized_universe = __sanitize_walk_universe(walk_universe)
 
-    filtered_universe = sanitized_universe[
-    __age_filter(sanitized_universe) & \
-    __zipcode_filter(sanitized_universe)
-    ]
+  filtered_universe = sanitized_universe[
+  __age_filter(sanitized_universe) & \
+  __zipcode_filter(sanitized_universe)
+  ]
 
-    return filtered_universe
+  return filtered_universe
 
 def __sanitize_walk_universe(walk_universe):
-    cleaned_universe = walk_universe.rename(
-      columns={
-      'Voter File VANID': 'van_id',
-      'Age': 'age', 'PrecinctName':
-      'precinct'
-      })
+  cleaned_universe = walk_universe.rename(
+    columns={
+    'Voter File VANID': 'van_id',
+    'Age': 'age', 'PrecinctName':
+    'precinct'
+    })
 
-    return cleaned_universe
+  return cleaned_universe
 
 def __age_filter(walk_universe):
   return (walk_universe.age >= FILTERS['min_age']) & \
