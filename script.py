ece567b269c5772d09f31025c28f86f9f4f68cfb
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2018-11-03 14:06:24 -0400

    Refactor script.py to distinguish public vs. private methods
    - These methods are not actually private, but we are denoting them as if they were
      + This highlights what our API actually is: #run and #voter_list

--- script.py
+++ script.py
@@ -11,12 +11,21 @@ def run():
   csv_name = CSV_NAME
 
   voter_df = pandas.read_csv(csv_name)
-  voter_df = sanitize_walk_universe(voter_df)
   voter_df = voter_list(voter_df)
 
   return voter_df
 
-def sanitize_walk_universe(walk_universe):
+def voter_list(walk_universe):
+    sanitized_universe = __sanitize_walk_universe(walk_universe)
+
+    filtered_universe = sanitized_universe[
+    __age_filter(sanitized_universe) & \
+    __zipcode_filter(sanitized_universe)
+    ]
+
+    return filtered_universe
+
+def __sanitize_walk_universe(walk_universe):
     cleaned_universe = walk_universe.rename(
       columns={
       'Voter File VANID': 'van_id',
@@ -26,18 +35,10 @@ def sanitize_walk_universe(walk_universe):
 
     return cleaned_universe
 
-def voter_list(walk_universe):
-    filtered_universe = walk_universe[
-    age_filter(walk_universe) & \
-    zipcode_filter(walk_universe)
-    ]
-
-    return filtered_universe
-
-def age_filter(walk_universe):
+def __age_filter(walk_universe):
   return (walk_universe.age >= FILTERS['min_age']) & \
   (walk_universe.age <= FILTERS['max_age'])
 
-def zipcode_filter(walk_universe):
+def __zipcode_filter(walk_universe):
   return walk_universe.mZip5.isin(FILTERS["zipcodes"])
 
